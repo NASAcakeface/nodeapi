1c45bef6cfff48cde0a9b24389e9b74849b4f73e
alexnaspo <alex.naspo@gmail.com>
2016-04-23 16:38:43 -0400

    photo upload to azure

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,24 @@
 'use strict';
+var azure = require('azure');
+var fs = require('fs');
 
 function defineRoutes(server) {
-    server.get("/face", function(req,res,next){
-        res.send(200, "FACE");
-        return next();
+    server.post('/upload', function (req, res) {
+        var accessKey = 'uGKV7EplzvL7hMkyk7InsBqiHDgeRH7DcyCJb/ZQAqbPm7bpzRTJ05WKYX7dzmiT0VCt2s6MgmgZYux/oq0oQQ==';
+        var storageAccount = 'nasahack';
+        var container = 'images';    
+        var blobService = azure.createBlobService(storageAccount, accessKey);
+        var photo =  req.files.photo;
+        // @TODO add timestamp
+        var stream = fs.createReadStream(photo.path);
+        blobService.createBlockBlobFromStream(container, photo.name, stream, photo.size, function (error) {
+            if (error) {
+                res.send(400, error)
+            } else {
+                res.send(201);
+            }
+        });       
     });
-
 }
 
 module.exports = defineRoutes;
