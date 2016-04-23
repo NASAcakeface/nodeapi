1a14c1bb7080bff93335a277d0b7201c2087ea1c
alexnaspo <alex.naspo@gmail.com>
2016-04-23 17:48:03 -0400

    cleanup

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,8 @@
 'use strict';
-var azure = require('azure');
-var fs = require('fs');
+var upload = require('../lib/upload');
 
 function defineRoutes(server) {
-    server.post('/upload/:userid', function (req, res, next) {
-        var accessKey = 'uGKV7EplzvL7hMkyk7InsBqiHDgeRH7DcyCJb/ZQAqbPm7bpzRTJ05WKYX7dzmiT0VCt2s6MgmgZYux/oq0oQQ==';
-        var storageAccount = 'nasahack';
-        var container = 'images';    
-        var blobService = azure.createBlobService(storageAccount, accessKey);
-        if(!req.files){
-            res.send(404, "photo required");
-            return next()
-        }
-        var photo =  req.files.photo;
-        var filename = req.params.userid + "_" + Date.now() + "_" + photo.name;
-        
-        var stream = fs.createReadStream(photo.path);
-
-        blobService.createBlockBlobFromStream(container, filename, stream, photo.size, function (error) {
-            stream.close()
-            if (error) {
-                res.send(400, error);
-            } else {
-                res.send(201);
-            }
-        });       
-    });
+    server.post('/upload/:userid', upload.uploadImage);
 }
 
 module.exports = defineRoutes;
