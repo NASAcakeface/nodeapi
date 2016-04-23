fd5e16d8905592e7fd5ac6b8b7cce210bff3c504
alexnaspo <alex.naspo@gmail.com>
2016-04-23 18:56:54 -0400

    upload file

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@
 var upload = require('../lib/upload');
 
 function defineRoutes(server) {
-    server.post('/upload/:input_type/:userid', upload.uploadImage);
+    server.post('/upload/:input_type/:userid', upload.uploadFile);
 }
 
 module.exports = defineRoutes;
